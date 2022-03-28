9ec178412da44459c6ba61c750eae8331572de28
mxt629 <xuantien.06021990@gmail.com>
2022-03-29 00:26:18 +0700

    Initial Commit

---
+++ SS16 Example2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SS16 Example2
+//
+//  Created by Little Chick on 29/03/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
